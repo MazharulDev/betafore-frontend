4c545a3595e269c3c472b2b691291452bca7e02d
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-11-17 21:34:42 +0600

    user registration login added

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,25 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Betafore",
-  description: "Simple authentication project",
-};
+"use client";
+import { isLoggedIn } from "@/services/auth.service";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "../components/loadingSpinner/Loading";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const userLoggedIn = isLoggedIn();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  useEffect(() => {
+    if (!userLoggedIn) {
+      router.push("/login");
+    }
+    setIsLoading(true);
+  }, [router, userLoggedIn]);
+  if (!isLoading) {
+    return <LoadingSpinner />;
+  }
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <section className="main-container">
-          <div className="w-full max-w-4xl">{children}</div>
-        </section>
-      </body>
-    </html>
+    <section className="main-container">
+      <div className="w-full max-w-4xl">{children}</div>
+    </section>
   );
 }
---
+++ src/types/common.ts
@@ -0,0 +1,30 @@
+export interface IMeta {
+  limit: number;
+  page: number;
+  total: number;
+}
+
+export type ResponseSuccessType = {
+  data: any;
+  meta?: IMeta;
+};
+export type IGenericErrorResponse = {
+  statusCode?: number;
+  message?: string;
+  errorMessages?: IGenericErrorMessage[];
+};
+
+export type IGenericErrorMessage = {
+  path: string | number;
+  message: string;
+};
+
+export type IUsers = {
+  _id: string;
+  name: string;
+  email: string;
+  image?: string;
+  role: "user";
+  phoneNumber: string;
+  password: string;
+};
