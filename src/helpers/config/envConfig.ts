01782b681ab0215573462009046c4d5b8da09bf6
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-11-19 08:24:20 +0600

    all required feature added

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,9 +1,5 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api/v1";
-};
-
-export const getImgbbAPI = (): string => {
-  return process.env.NEXT_PUBLIC_API_IMGBB_TOKEN as string;
+  return process.env.NEXT_PUBLIC_API_BASE_URL as string;
 };
 
 export const stripePubKey = (): string => {
